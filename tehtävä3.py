f968d35ae606dc23c0b7c92ddb4decf4c035d293
Eero Pokki <eero.pokki@gmail.com>
2025-08-20 15:29:15 +0300

    2. Muuttujat ja vuorovaikutteiset ohjelmat tehtävä3

---
+++ tehtävä3.py
@@ -0,0 +1,8 @@
+
+a = float(input('Mikä on suorakulmion kanta? '))
+h = float(input('Mikä on suorakulmion korkeus? '))
+
+p = 2 * a + 2 * h
+A = a * h
+print(f'Pinta-ala = {A}')
+print(f'Piiri = {p}')
