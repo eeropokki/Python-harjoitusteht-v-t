33a3c4c65cc6162c3f9ac6d5e7637d2020c04093
Eero Pokki <eero.pokki@gmail.com>
2025-08-21 15:21:07 +0300

    2. Muuttujat ja vuorovaikutteiset ohjelmat tehtävä6

---
+++ tehtävä6.py
@@ -0,0 +1,19 @@
+import random
+
+kolmenro1 = random.randint(0,9)
+kolmenro2 = random.randint(0,9)
+kolmenro3 = random.randint(0,9)
+
+nelinro1 = random.randint(1,6)
+nelinro2 = random.randint(1,6)
+nelinro3 = random.randint(1,6)
+nelinro4 = random.randint(1,6)
+
+
+kolmenrokoodi = (f'{kolmenro1}{kolmenro2}{kolmenro3}')
+nelinrokoodi = (f'{nelinro1}{nelinro2}{nelinro3}{nelinro4}')
+
+print(kolmenrokoodi)
+print(nelinrokoodi)
+
+
