726ce22ef118b46943c6b3a217c81caa1011a6dd
Eero Pokki <eero.pokki@gmail.com>
2025-08-27 20:59:06 +0300

    mod3 harjoitustehtävä 4

---
+++ mod3/harjoitustehtävät/teht4.py
@@ -0,0 +1,10 @@
+vuosi = int(input("Anna vuosiluku: "))
+
+if vuosi % 400 == 0:
+    print(vuosi, "on karkausvuosi.")
+elif vuosi % 100 == 0:
+    print(vuosi, "ei ole karkausvuosi.")
+elif vuosi % 4 == 0:
+    print(vuosi, "on karkausvuosi.")
+else:
+    print(vuosi, "ei ole karkausvuosi.")
