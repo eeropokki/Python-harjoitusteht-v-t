e7a5c9d3d070b3c97a464a1b6ae33cb8f011268b
Eero Pokki <eero.pokki@gmail.com>
2025-09-04 19:17:15 +0300

    mod5 harjoitustehtävä 4

---
+++ Mod5/harjoitustehtävät/teht4.py
@@ -0,0 +1,11 @@
+kaupungit = []
+
+for i in range(5):
+    kaupunki = input("Kirjoita joku kaupunki: ")
+    print(kaupunki)
+    print("")
+    kaupungit.append(kaupunki)
+
+##print(kaupungit)
+for kaupunki in kaupungit:
+    print(kaupunki)
