0abdebb33be8cb62f0b516c8c0e49e8186fda3a2
Eero Pokki <eero.pokki@gmail.com>
2025-10-21 17:58:26 +0300

    class auto ja mod9 teht1

---
+++ Mod9/harjoitustehtävät/classes/auto.py
@@ -0,0 +1,20 @@
+class Auto:
+
+    def __init__(self, new_rekisteritunnus, new_huippunopeus):
+        self.rekisteritunnus = new_rekisteritunnus
+        self.huippunopeus = new_huippunopeus
+        self.nopeus = 0
+        self.matka = 0
+
+    def kiihdytä(self, arvo):
+        pass
+        # Lisää arvo self nopeuteen
+        # Tarkasta ettei nopeus ole yli huippunopeus. Jos on, aseta nopeus = huippunopeus.
+        # Tarkasta ettei nopeus ole alle 0. Jos on, aseta nopeus = 0.
+
+    def kulje(self, aika):
+        pass
+        # Laske kuinka paljon auto on kulkenut annetussa ajassa tietyllä nopeudella.
+        # Lisää kuljettu matka kokonaismatkaan.
+
+
---
+++ Mod9/harjoitustehtävät/teht1.py
@@ -0,0 +1,12 @@
+from classes.auto import Auto
+
+autot = []
+
+auto1 = Auto("ABC-123", 142)
+
+print(f"Rekisteritunnus: {auto1.rekisteritunnus} "
+      f"Huippunopeus: {auto1.huippunopeus} km/h "
+      f"Nopeus: {auto1.nopeus} km/h "
+      f"Matka: {auto1.matka} m ")
+
+
